771424464a8098ce94c75284f5094aa1f11c66a4
sbx_user1051 <sbx_user1051@169.254.16.21>
2020-02-27 05:36:43 +0000

    Thu Feb 27 05:36:43 UTC 2020

--- src/main/java/befaster/solutions/HLO/Main.java
+++ src/main/java/befaster/solutions/HLO/Main.java
@@ -1,4 +1,10 @@
 package befaster.solutions.HLO;
 
 public class Main {
+    public static void main(String[] args) {
+        HelloSolution h = new HelloSolution();
+        String s = h.hello("da");
+        System.out.println(s);
+    }
 }
+
