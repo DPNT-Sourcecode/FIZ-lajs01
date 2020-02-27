ac321a20013c5c24161a4e35e3468617e75a1f6b
sbx_user1051 <sbx_user1051@169.254.16.21>
2020-02-27 05:55:19 +0000

    Thu Feb 27 05:55:19 UTC 2020

--- src/main/java/befaster/solutions/FIZ/Main.java
+++ src/main/java/befaster/solutions/FIZ/Main.java
@@ -2,7 +2,14 @@ package befaster.solutions.FIZ;
 
 public class Main {
     public static void main(String[] args) {
-
+        FizzBuzzSolution f = new FizzBuzzSolution();
+        String s = f.fizzBuzz(6);
+        System.out.println(s);
+        s = f.fizzBuzz(10);
+        System.out.println(s);
+        s = f.fizzBuzz(2);
+        System.out.println(s);
     }
 }
+
 
