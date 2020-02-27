92f6bdb78e4c35afbdfe12c3fff9fbe6ee2dfe12
sbx_user1051 <sbx_user1051@169.254.16.21>
2020-02-27 05:52:43 +0000

    Thu Feb 27 05:52:43 UTC 2020

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -3,9 +3,40 @@ package befaster.solutions.FIZ;
 import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
+    public boolean checkThree(Integer number){
+        if(number % 3 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkFive(Integer number){
+        if(number % 5 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public String print(boolean three, boolean five){
+        if(three == true && five == false){
+            return "fizz";
+        } else if(three == false && five == true){
+            return "buzz";
+        } else if(three == true && five == true){
+            return "fizz buzz";
+        } else {
+            return "n";
+        }
+    }
 
     public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+        String solutie = print(checkThree(number),checkFive(number));
+        if(solutie.equals("n")){
+            return number.toString();
+        } else {
+            return solutie;
+        }
     }
 
 }
+
--- src/main/java/befaster/solutions/FIZ/Main.java
+++ src/main/java/befaster/solutions/FIZ/Main.java
@@ -2,6 +2,7 @@ package befaster.solutions.FIZ;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
+
